9839cfc98005b763b37fc125dbdb8036083bac1d
NDNewell <rebelexplorer31@gmail.com>
2015-10-10 20:47:41 +0100

    chore: Add grunt associated files and renamed files

---
+++ grunt_tasks/html.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt, config) {
+    grunt.config.merge({
+        processhtml: {
+            dist: {
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html' : 'foo/bar.html',
+                }
+            }
+        },
+        watch: {
+            processhtml: {
+                files: 'src/index.html',
+                tasks: 'processhtml'
+            },
+            /*htmlmin: {
+                files: 'foo/bar.html',
+                tasks: 'htmlmin'
+            }*/
+        }
+    });
+};
---
+++ grunt_tasks/image.js
@@ -0,0 +1,14 @@
+module.exports = function (grunt, config) {
+    grunt.config.merge({
+        respimg: {
+            default: {
+                files: [{
+                    expand: true,
+                    cwd: config.imgSrcDir,
+                    src: ['*.{.gif,jpg,png,svg}'],
+                    dest: config.imgResDir
+                }],
+            }
+        }
+    });
+};

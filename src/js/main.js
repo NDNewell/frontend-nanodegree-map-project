423f9ba33508dcb0219a696715b2fdf4cd15a0a4
NDNewell <rebelexplorer31@gmail.com>
2016-01-23 15:01:14 +0000

    feat: Detect if device supports touch, convert hover class with active

--- src/js/main.js
+++ src/js/main.js
@@ -508,12 +508,10 @@ function AppViewModel () {
             // Filter locations that match the user's favorites
             // When a match is found, add a class to style it as 'selected'
             if(userFavorites.indexOf($breakName) > -1) {
-                $favoriteWrapper.removeClass('not-a-favorite');
-                $favoriteWrapper.addClass('is-a-favorite');
+                $favoriteWrapper.removeClass('not-a-favorite').addClass('is-a-favorite');
             } else {
                 if($favoriteWrapper.hasClass('is-a-favorite')) {
-                    $favoriteWrapper.addClass('not-a-favorite');
-                    $favoriteWrapper.removeClass('is-a-favorite');
+                    $favoriteWrapper.addClass('not-a-favorite').removeClass('is-a-favorite');
                 };
             };
         });
@@ -2378,6 +2376,44 @@ function AppViewModel () {
         return cl;
     };
 
+    /**
+    * Converts :hover CSS to :active CSS on mobile devices.
+    * Otherwise, when tapping a button on a mobile device, the button stays in
+    * the :hover state until the button is pressed.
+    *
+    * Inspired by: https://gist.github.com/rcmachado/7303143
+    * @author  Michael Vartan <michael@mvartan.com>
+    * @version 1.0
+    * @date    2014-12-20
+    */
+    // Check if the device supports touch events
+    if('ontouchstart' in document.documentElement) {
+
+      console.log('device supports touch events');
+
+      // Loop through each stylesheet
+      for(var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
+        var sheet = document.styleSheets[sheetI];
+
+        // Verify if cssRules exists in sheet
+        if(sheet.cssRules) {
+
+          // Loop through each rule in sheet
+          for(var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
+
+            var rule = sheet.cssRules[ruleI];
+
+            // Verify rule has selector text
+            if(rule.selectorText) {
+
+              // Replace hover psuedo-class with active psuedo-class
+              rule.selectorText = rule.selectorText.replace(":hover", ":active");
+            };
+          };
+        };
+      };
+    };
+
     /* In screen sizes greater than 767px horizontal scrolling is enabled
     during map view */
     self.enableHorizontalScrolling = function () {
@@ -3539,7 +3575,6 @@ function AppViewModel () {
                 $this.removeClass("is-a-favorite-guide").addClass("not-a-favorite-guide");
                 console.log("unfavorite " + breakName);
                 removeFavorite(breakName);
-                $this.blur();
 
             } else {
 
